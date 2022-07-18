5fb5effbaa967c6df993bf0738cb8059b60ad4e6
InMDev <inzaghi.moniaga@gmail.com>
2022-07-18 14:12:34 +0800

    pianobot DONE

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import Member from './components/Team/Member';
 import Team from './components/Team/Team'
 
+import Piano from './components/Projects/Pianobot';
+
 function App() {
   return (
-    <div className="App flex flex-row">
-       <Team/>
+    <div className="App">
+       <Piano />
     </div>
   );
 }
