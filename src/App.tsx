c90665bf4eefae346910d812402173b963526cd2
Sashank Shukla <ayaz.shukla@gmail.com>
2022-07-18 17:38:32 -0700

    tested deleting css, failed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Member from './components/Team/Member';
 import Team from './components/Team/Team'
-import Piano from './components/Projects/Pianobot';
 import Navbar from './components/Navbar/Navbar';
 import Contact from './components/ContactPage/ContactPage';
 
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,18 +5,18 @@ import Logo from '../Navbar/logo.png';
 const Navbar = () =>{
   const [state, setState] = useState(false)
   const navigation = [
-    { title: "Home", path: "javascript:void(0)" },
-    { title: "Projects", path: "javascript:void(0)" },
-    { title: "Our Team", path: "javascript:void(0)" },
-    { title: "Join Us", path: "javascript:void(0)" },
-    { title: "Sponsors", path: "javascript:void(0)" }
+    { title: "Home", path: "#" },
+    { title: "Projects", path: "#" },
+    { title: "Our Team", path: "#" },
+    { title: "Join Us", path: "#" },
+    { title: "Sponsors", path: "#" }
 ]
   return (
     <section className='w-full mb-8 font-Roboto300 fixed z-20'>
       <nav className="bg-white w-full border-b md:border-0 md:static">
           <div className="items-center px-4 max-w-screen-xl mx-auto md:flex md:px-8">
               <div className="flex items-center justify-between py-3 md:py-5 md:block">
-                    <a href="javascript:void(0)">
+                    <a href="#">
                         <img
                             src={Logo}
                             width={40} 
@@ -42,7 +42,7 @@ const Navbar = () =>{
                       </button>
                   </div>
               </div>
-              <div className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${ state ? 'block' : 'hidden'}`}>
+              <div className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 block`}>
                   <ul className="justify-center items-center space-y-8 md:flex md:space-x-6 md:space-y-0">
                       {
                           navigation.map((item, idx) => {
@@ -58,7 +58,7 @@ const Navbar = () =>{
                   </ul>
               </div>
               <div className="hidden md:inline-block">
-                <a href="javascript:void(0)" className="py-3 px-4 border-2 border-red-600 hover:bg-red-600/25 text-gray-900 font-bold rounded-md shadow">
+                <a href="#" className="py-3 px-4 border-2 border-red-600 hover:bg-red-600/25 text-gray-900 font-bold rounded-md shadow">
                     Donate
                 </a>
               </div>
