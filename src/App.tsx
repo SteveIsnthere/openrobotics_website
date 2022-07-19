bcd4a659100d7fa2557e2dfb41a84097fc2679bc
Sashank Shukla <ayaz.shukla@gmail.com>
2022-07-19 00:20:39 -0700

    footer complete, recruitment interface and construct complete

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,16 @@ import Member from './components/Team/Member';
 import Team from './components/Team/Team'
 import Navbar from './components/Navbar/Navbar';
 import Contact from './components/ContactPage/ContactPage';
+import Recruitment from './components/Recruitment/Recruitment';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
-    <div className="App w-screen overflow-x-hidden font-Roboto">
+    <div className="App w-screen overflow-x-hidden font-Roboto selection:bg-yellow-400 selection:text-gray-900">
        <Navbar/>
        <Contact/>
+       <Team/>
+       <Footer/>
     </div>
   );
 }
--- src/components/Recruitment/Recruitment.tsx
+++ src/components/Recruitment/Recruitment.tsx
@@ -1,8 +1,37 @@
 import React from 'react'
 
-function Recruitment() {
+
+class Position{
+  title:string;
+  positions:number;
+  team:string;
+  description:string;
+
+  constructor(title:string, positions:number,description:string,team:string){
+    this.title = title;
+    this.positions = positions;
+    this.team = team;
+    this.description = description;
+  }
+}
+
+const Recruitment = () =>{
+  const positions:Array<Position> = [
+    new Position("Full Stack Developer", 2, "yuh","Admin"),
+    new Position("Software Engineer", 3, "Do something","RoboCup"),
+    new Position("Mechanical Engineer" , 5, "YEHJDFA","PianoBot"),];
+
   return (
-    <div>Recruitment</div>
+    <>
+       {positions.map((position:Position) => (
+         <div className='flex flex-row my-4 mt-20 w-screen'>
+           <h1 className='text-gray-900'>{position.title}</h1>
+           <h1 className='text-gray-900'>{position.positions}</h1>
+           <h1 className="text-gray-900">{position.team}</h1>
+           <h1 className='text-gray-900'>{position.description}</h1>
+         </div>
+       ))}
+    </>
   )
 }
 
