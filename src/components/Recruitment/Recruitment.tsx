be49d10f0d9297853095d5a854db9423639d8c67
Sashank Shukla <ayaz.shukla@gmail.com>
2022-07-19 17:50:01 -0700

    changed default prof(needs approval), fixed dropdown recruitment component

--- src/components/Recruitment/Recruitment.tsx
+++ src/components/Recruitment/Recruitment.tsx
@@ -28,22 +28,33 @@ const Recruitment = () =>{
     return visible ? "hidden" : "visible";
   }
 
+  const removeBorder = (visible:boolean) => {
+    return !visible ? "rounded-b-none" : " ";
+  }
+
+  const border2 = (visible: boolean) => {
+    return !visible ? "rounded-t-none" : " ";
+  }
+
   return (
     <section className='flex flex-col m-[10%] py-10 md:mt-24 sm:mt-60 sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg justify-center items-center'>
          <h1 className='text-3xl py-4 font-extrabold md:text-5xl text-gray-900'>Current Openings</h1>
+         <span className="w-64 h-[2px] mt-0 bg-gray-900 mb-6"></span>
          <h1 className='text-2xl py-2 font-light md:text-4xl text-gray-700'>Thank you for your interest!</h1>
-         <p className='py-4 text-md md:text-xl  text-gray-500'>Our Fall Recruitment cycle is in full swing right now! If you're interested in joining, apply for a technical position here. Note that our applications are open to students across all faculties and of varying skill levels. We look forward to your applications!</p>
+         <p className='py-4 text-md md:text-xl  text-gray-500'>Our Fall Recruitment cycle is in full swing right now! If you're interested in joining, apply for a technical position here. Note that our applications are open to students across all faculties and of varying skill levels. We look forward to your applications!
+          For further questions please <span className="font-bold text-red-500 cursor-pointer hover:text-red-700">contact</span> us
+         </p>
          {positions.map((position:Position) => (
           <div className='flex flex-col w-full py-4'>
-             <div className='flex py-4 mt-4 flex-row justify-between text-center items-start font-bold rounded-lg bg-gray-200 text-gray-900 w-full'>
+             <div className={'flex py-4 mt-4 flex-row justify-between text-center items-start font-bold rounded-lg border-b-2 border-l-2 border-r-2 border-gray-200 bg-gray-200 text-gray-900 w-full ' + removeBorder(arrow)}>
             <h1 className='mx-2'>{position.title}</h1>
             <button className='outline-none style-none border-none bg-gray-200' onClick={() => setArrow(!arrow)}>
              {arrow ? <AiOutlineDown className='mx-4 my-1 font-bold'/> : <AiOutlineUp className='mx-4 my-1 font-bold'/>}
             </button>
             </div>
-            <div className={"w-full text-center flex flex-col justify-center items-center rounded-lg py-4 bg-white border-2 border-t-0 border-gray-200 mt-0 " + set(arrow)}>
-              <p className='text-gray-900'>Available positions : {position.positions}</p>
-              <p className='text-gray-900'>About the role : {position.description}</p>
+            <div className={"w-full text-center flex flex-col justify-start items-start rounded-lg py-4 bg-white border-2 border-gray-100 border-t-gray-200 mt-0 " + set(arrow) + " " + border2(arrow)}>
+              <p className='text-gray-900 font-bold mb-4 px-4 py-4'>Number of positions : <span className="font-normal text-gray-900">{position.positions}</span></p>
+              <p className='text-gray-900 font-bold px-4'>About the role : <span className="font-normal text-gray-900">{position.description}</span></p>
             </div>
           </div>
        ))}
--- src/components/Team/Team.tsx
+++ src/components/Team/Team.tsx
@@ -35,9 +35,9 @@ const Team = () => {
     new Person("Kritika Joshi"   , 'Integration Lead' ,     'im' , 'im' , 'assets/img/admin/kritika.png'   , 'im'),
     new Person("Niki Manesh"     , 'Mentor'           ,     'im' , 'im' , 'assets/img/admin/niki.png'      , 'im'),
     new Person("Brian Zimmerman" , 'Finance Lead'     ,     'im' , 'im' , 'assets/img/admin/brian.png'     , 'im'),
-    new Person("Nafae Saiyed"    , 'Finance Member'   ,     'im' , 'im' , 'assets/img/default_profile.png' , 'im'),
-    new Person("Taymur Pasha"    , 'Finance Member'   ,     'im' , 'im' , 'assets/img/default_profile.png' , 'im'),
-    new Person("Jay Hou"         , 'Finance Member'   ,     'im' , 'im' , 'assets/img/default_profile.png' , 'im'),
+    new Person("Nafae Saiyed"    , 'Finance Member'   ,     'im' , 'im' , 'assets/img/default_profile.jpg' , 'im'),
+    new Person("Taymur Pasha"    , 'Finance Member'   ,     'im' , 'im' , 'assets/img/default_profile.jpg' , 'im'),
+    new Person("Jay Hou"         , 'Finance Member'   ,     'im' , 'im' , 'assets/img/default_profile.jpg' , 'im'),
     new Person("Tian Hao Xu"     , 'Safety Officer'   ,     'im' , 'im' , 'assets/img/admin/tian.png'      , 'im'),
   ];
   const PianoBot:Array<Person> = [
@@ -53,28 +53,28 @@ const Team = () => {
   ];
   const ArtBot:Array<Person> = [
     new Person("Aiden Smith"      , 'ArtBot Lead'   , 'im' , 'im' , 'assets/img/artbot/aiden.png'    , 'im'),
-    new Person("Navneeth Devadas" , 'ArtBot Member' , 'im' , 'im' , 'assets/img/default_profile.png' , 'im'),
-    new Person("Felice Jie Chen"  , 'ArtBot Member' , 'im' , 'im' , 'assets/img/default_profile.png' , 'im'),
+    new Person("Navneeth Devadas" , 'ArtBot Member' , 'im' , 'im' , 'assets/img/default_profile.jpg' , 'im'),
+    new Person("Felice Jie Chen"  , 'ArtBot Member' , 'im' , 'im' , 'assets/img/default_profile.jpg' , 'im'),
     new Person("Benji Li"         , 'ArtBot Member' , 'im' , 'im' , 'assets/img/artbot/benji.png'    , 'im'),
   ];
   const Arm:Array<Person> = [
     new Person("Joey Gaffney"     , 'Arm Lead'   , 'im' , 'im' , 'assets/img/arm/joey.png'        , 'im'),
-    new Person("Arwin Patrick"    , 'Arm Lead'   , 'im' , 'im' , 'assets/img/default_profile.png' , 'im'),
+    new Person("Arwin Patrick"    , 'Arm Lead'   , 'im' , 'im' , 'assets/img/default_profile.jpg' , 'im'),
     new Person("Jonas Chianu"     , 'Arm Member' , 'im' , 'im' , 'assets/img/arm/jonas.png'       , 'im'),
     new Person("Hazel Chongoti"   , 'Arm Member' , 'im' , 'im' , 'assets/img/arm/hazel.png'       , 'im'),
     new Person("David Kim"        , 'Arm Member' , 'im' , 'im' , 'assets/img/arm/david.png'       , 'im'),
     new Person("Debby Lin"        , 'Arm Member' , 'im' , 'im' , 'assets/img/arm/debby.png'       , 'im'),
-    new Person("Cailin Ringstorm" , 'Arm Member' , 'im' , 'im' , 'assets/img/default_profile.png' , 'im'),
-    new Person("Iris Xu"          , 'Arm Member' , 'im' , 'im' , 'assets/img/default_profile.png' , 'im'),
+    new Person("Cailin Ringstorm" , 'Arm Member' , 'im' , 'im' , 'assets/img/default_profile.jpg' , 'im'),
+    new Person("Iris Xu"          , 'Arm Member' , 'im' , 'im' , 'assets/img/default_profile.jpg' , 'im'),
     new Person("Ryan Yeung"       , 'Arm Member' , 'im' , 'im' , 'assets/img/arm/ryan.png'        , 'im'),
-    new Person("Grace Zhang"      , 'Arm Member' , 'im' , 'im' , 'assets/img/default_profile.png' , 'im'),
+    new Person("Grace Zhang"      , 'Arm Member' , 'im' , 'im' , 'assets/img/default_profile.jpg' , 'im'),
   ];
   const Train:Array<Person> = [
     new Person("Kuok Kun(AO) Chio" , 'RoboCup Lead'   , 'im' , 'im' , 'assets/img/train/kuok.png'      , 'im'),
     new Person("Yang Su"           , 'RoboCup Lead'   , 'im' , 'im' , 'assets/img/train/yang.png'      , 'im'),
     new Person("Kamran Alam"       , 'RoboCup Member' , 'im' , 'im' , 'assets/img/train/kamran.png'    , 'im'),
     new Person("Wilson Chen"       , 'RoboCup Member' , 'im' , 'im' , 'assets/img/train/wilson.png'    , 'im'),
-    new Person("Vishal Desh"       , 'RoboCup Member' , 'im' , 'im' , 'assets/img/default_profile.png' , 'im'),
+    new Person("Vishal Desh"       , 'RoboCup Member' , 'im' , 'im' , 'assets/img/default_profile.jpg' , 'im'),
     new Person("Yoshi Inomata"     , 'RoboCup Member' , 'im' , 'im' , 'assets/img/train/yoshi.png'     , 'im'),
     new Person("Eros Rojas"        , 'RoboCup Member' , 'im' , 'im' , 'assets/img/train/eros.png'      , 'im'),
     new Person("Tian Hao Xu"       , 'RoboCup Member' , 'im' , 'im' , 'assets/img/train/tian.png'      , 'im'),
@@ -83,14 +83,14 @@ const Team = () => {
     new Person("Matthew Sam"   , 'Gripper Lead'   , 'im' , 'im' , 'assets/img/gripper/matthew.png' , 'im'),
     new Person("Husein Alatas" , 'Gripper Member' , 'im' , 'im' , 'assets/img/gripper/husein.png'  , 'im'),
     new Person("Akhil Prabhu"  , 'Gripper Member' , 'im' , 'im' , 'assets/img/gripper/akhil.png'   , 'im'),
-    new Person("Chaewoon Song" , 'Gripper Member' , 'im' , 'im' , 'assets/img/default_profile.png' , 'im'),
-    new Person("Jenica Sy"     , 'Gripper Member' , 'im' , 'im' , 'assets/img/default_profile.png' , 'im'),
+    new Person("Chaewoon Song" , 'Gripper Member' , 'im' , 'im' , 'assets/img/default_profile.jpg' , 'im'),
+    new Person("Jenica Sy"     , 'Gripper Member' , 'im' , 'im' , 'assets/img/default_profile.jpg' , 'im'),
   ];
   const Software:Array<Person> = [
     new Person("Francisco Farinha" , 'Software Lead'   , 'im' , 'im' , 'assets/img/software/francisco.png' , 'im'),
     new Person("Andrew Xie"        , 'Software Lead'   , 'im' , 'im' , 'assets/img/software/andrew.png'    , 'im'),
     new Person("Aayush Behl"       , 'Software Member' , 'im' , 'im' , 'assets/img/software/aayush.png'    , 'im'),
-    new Person("Ashli Forbes"      , 'Software Member' , 'im' , 'im' , 'assets/img/default_profile.png'    , 'im'),
+    new Person("Ashli Forbes"      , 'Software Member' , 'im' , 'im' , 'assets/img/default_profile.jpg'    , 'im'),
     new Person("Damien Fung"       , 'Software Member' , 'im' , 'im' , 'assets/img/software/damien.png'    , 'im'),
     new Person("Tawsif Hasan"      , 'Software Member' , 'im' , 'im' , 'assets/img/software/tawsif.png'    , 'im'),
   ];
@@ -100,8 +100,6 @@ const Team = () => {
     <section className="bg-white w-screen overscroll-none mt-16 pt-10 z-10">
       <div className="container">
           <h1  className="text-4xl mx-[5%] font-extrabold font-Roboto700 mb-20 text-gray-800 capitalize lg:text-5xl">Meet The Team</h1>  
-
-
           <div className='my-16 mx-[5%] mb-24'>
             <h1  className="text-3xl font-lighter md:font-bold text-gray-800 capitalize -mb-4 lg:text-4xl">Leads</h1>
             <div className="grid grid-cols-1 mt-12 gap-8 xl:mt-16 md:grid-cols-2 xl:grid-cols-4">
