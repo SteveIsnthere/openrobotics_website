816d14a044d9712c894d69b1f8c3250ddc7eab53
Sashank Shukla <ayaz.shukla@gmail.com>
2022-08-22 20:22:57 -0700

    logo href

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -18,7 +18,7 @@ const Navbar = () => {
       <nav className="bg-white w-full border-b md:border-0 md:static">
         <div className="items-center px-4 max-w-screen-xl mx-auto md:flex md:px-8">
           <div className="flex items-center justify-between py-3 md:py-5 md:block">
-            <a href="#">
+            <a href="/">
               <img src={Logo} width={40} height={50} alt="OpBots Logo" />
             </a>
             <div className="md:hidden">
